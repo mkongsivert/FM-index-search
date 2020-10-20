b270c5e3a6aa5eb127efc8ec95ea55b4a6c92c40
mkongsivert <mkongsivert@cs.hmc.edu>
2020-10-20 11:59:01 -0400

    Initial structure (based on ChunkyString)

---
+++ bitvector.cpp
@@ -0,0 +1,78 @@
+/**
+ * \file chunkystring.cpp
+ *
+ * \authors the fantastical falcon_kangaroo
+ *
+ * \brief Implements the ChunkyString class.
+ */
+
+#include "bitvector.hpp"
+
+#include <string>
+#include <sstream>
+
+rank_support::rank_support(bit_vector bits) :
+        size_{0}
+{
+    
+}
+
+ChunkyString::iterator ChunkyString::begin()
+{
+    return iterator(0, chunks_.begin());
+}
+
+// ChunkyString& ChunkyString::operator+=(const ChunkyString& rhs)
+// {
+//     // If this is empty, use the assignment operator to copy rhs into this
+//     if(this->size_ == 0) {
+//         *this = rhs;
+//         return *this;
+//     }
+
+//     // create a copy of rhs
+//     ChunkyString newRhs{rhs};
+
+//     // save an iterator to one before the current end of chunks
+//     chunklist_iter_t seam = --chunks_.end();
+
+//     // append the two lists
+//     chunks_.splice(chunks_.end(), newRhs.chunks_);
+
+//     // check if the two chunks at the seam of the two lists can merge
+//     // and if so, merge them
+//     tryMergeRight(seam);
+
+//     // update size
+//     size_ += rhs.size_;
+
+//     return *this;
+// }
+
+// bool ChunkyString::operator==(const ChunkyString& rhs) const
+// {
+//     if(size_ != rhs.size_) {
+//         return false;
+//     }
+
+//     for(auto it = begin(), rhsIt = rhs.begin(); it != end(); ++it, ++rhsIt) {
+//         if(*it != *rhsIt) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// bool ChunkyString::operator!=(const ChunkyString& rhs) const
+// {
+//     return !(*this == rhs);
+// }
+
+// std::ostream& ChunkyString::print(std::ostream& out) const
+// {
+//     for(auto it = begin(); it != end(); ++it) {
+//         out << *it;
+//     }
+
+//     return out;
+// }
---
+++ bitvector.hpp
@@ -0,0 +1,115 @@
+/**
+ * \file chunkystring.hpp
+ *
+ * \authors CS 70 given code, with additions by the fantastical falcon_kangaroo
+ *
+ * \brief Declares the ChunkyString class.
+ */
+
+#ifndef BITSTRING_HPP_INCLUDED
+#define BITSTRING_HPP_INCLUDED 1
+
+#include <cstddef>
+#include <string>
+#include <list>
+#include <iterator>
+#include <iostream>
+#include <type_traits>
+
+/**
+ * \class BitString
+ * \brief Represents a string of bits
+ *
+ * \details TODO
+ *
+ * \remarks
+ *   TODO
+ */
+class bit_vector {
+    // Forward declaration of private class.
+
+public:
+    /**
+     * \brief Default constructor
+     *
+     * \note Runs in constant time.
+     */
+    bit_vector();
+    // The synthesized copy constructor, assignment operator, and destructor
+    // should be sufficient, because std::list uses some sort of allocator,
+    // and is therefore responsible for its own memory.
+
+private:
+    std::unique_ptr<std::byte[]> bytes_;
+
+    /**
+     * \class rank_support
+     * \brief Class / structure that “wraps” the underlying bit-vector.
+     */
+    //template <typename Element, typename ChunklistIterator>
+};
+
+class rank_support {
+public:
+        
+    /// Returns a singular value.
+    /// \note It is undefined behavior to use this for anything but
+    ///       assigning from another iterator.
+    rank_support(bit_vector bits);
+
+    /**
+    * \brief Split a chunk into two different chunks.
+    *
+    * \param it   An iterator to the character that will become the first
+    *              character of the new chunk.
+    *
+    * \remarks
+    *      This will call tryMergeRight() on the newly-inserted chunk.
+    */
+    uint64_t rank1(uint64_t i);
+    uint64_t rank0(uint64_t i);
+    uint64_t overhead();
+    void save(string& fname);
+    void load(string& fname);
+
+private:
+    friend class bit_vector;
+
+    /**
+     * \brief
+     *      The size of the associated bit vector
+     */
+    size_t size_;
+
+    /**
+    * \brief
+    *       An array to help compute rank
+    */
+    std::unique_ptr<size_t[]> Rs_;
+
+    /**
+    * \brief
+    *       An array to help compute rank
+    */
+    std::unique_ptr<size_t[]> Rb_;
+
+    /**
+    * \brief
+    *       An array to help compute rank
+    */
+    std::unique_ptr<size_t[]> Rp_;
+
+};
+
+/**
+ * \brief Print operator.
+ * \remarks
+ *   Like the ones above, it's just a wrapper around a member function
+ *   that does the actual work, and we don't mind if people know that.
+ *
+inline std::ostream& operator<<(std::ostream& out, const ChunkyString& text)
+{
+    return text.print(out);
+}*/
+
+#endif // BITSTRING_HPP_INCLUDED
